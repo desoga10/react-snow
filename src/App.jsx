06d447d5fa4359bfd914b39b040ec65287cce2eb
deji adesoga <done2stay@gmail.com>
2021-07-09 23:39:07 +0100

    Made Changes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img
+          src="https://res.cloudinary.com/dz4tt9omp/image/upload/v1625528354/react.png"
+          alt="logo"
+        />
+        <p>React 18 Alpha Setup Deployed on Vercel with SnowPack</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
